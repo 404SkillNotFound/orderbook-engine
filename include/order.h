009fccdff975b5d281e7a7efdd53f10170e696f4
404SkillNotFound <shivang.upadhyay1@gmail.com>
2026-05-04 20:04:47 +0530

    add Order struct and basic OrderBook with addOrder

--- include/order.h
+++ include/order.h
@@ -1,1 +1,9 @@
-struct 
+#pragma once
+
+struct Order
+{
+    int id;
+    double price;
+    int quantity;
+    char side; // B or S (Buy or Sell)
+};
---
+++ include/orderbook.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <map>
+#include <list>
+#include <functional>
+#include <iostream>
+#include "order.h"
+
+class OrderBook
+{
+    std::map<double, std::list<Order>, std::greater<double>> bids; // highest price
+    std::map<double, std::list<Order>> asks;                       // lowest price
+public:
+    void addOrder(const Order &o)
+    {
+        if (o.side == 'B')
+        {
+            bids[o.price].push_back(o);
+        }
+        else if (o.side == 'S')
+        {
+            asks[o.price].push_back(o);
+        }
+        else
+        {
+            std::cout << "Kindly choose 'B' or 'S'\n";
+        }
+    }
+};
