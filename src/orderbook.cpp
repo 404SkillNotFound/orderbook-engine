f5449e1053dc5816050ac4fa81b9c27f64a3f917
404SkillNotFound <shivang.upadhyay1@gmail.com>
2026-05-10 18:18:37 +0530

    add quantity matching and FIFO order execution

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -26,16 +26,39 @@ void OrderBook::matchOrder()
 {
     if (bids.empty() || asks.empty())
     {
-        std::cout << "Sorry! We do not have enough number of orders to match yor up, try again later.\n";
+        std::cout << "Sorry! We do not have enough number of orders to match you up, try again later.\n";
         return;
     }
     auto bestBid = bids.begin();        // first key-value pair of bids
-    double priceOfBid = bestBid->first; // first key of bids entry
+    double priceOfBid = bestBid->first; // first key of 'bids' entry
+
     auto bestAsk = asks.begin();
-    double priceOfAsk = bestAsk->first;
+    double priceOfAsk = bestAsk->first; // first key of 'asks' entry
+
     if (priceOfBid >= priceOfAsk) // Bid = Buyer, Ask = Seller
     {
         std::cout << "Trade Possible.\n";
+
+        // map entry: first = price level, second = list<Order> at that price
+        auto &buyOrder = (bestBid->second).front(); // front() gives oldest order at this price level (FIFO)
+
+        auto &sellOrder = (bestAsk->second).front(); // front() gives oldest order at this price level (FIFO)
+
+        int tradeQuantity = std::min(buyOrder.quantity, sellOrder.quantity); // actual shares traded = smaller quantity between buyer and seller
+        buyOrder.quantity -= tradeQuantity;
+        sellOrder.quantity -= tradeQuantity; // reduce remaining quantities after executing the trade
+
+        if (buyOrder.quantity == 0)
+        {
+            // we pop that Order from the bestBid, as now the order is executed
+            (bestBid->second).pop_front();
+        }
+        if (sellOrder.quantity == 0)
+        {
+            // we pop that Order from the bestAsk, as now the order is exceuted
+            (bestAsk->second).pop_front();
+        }
+        // Todo: Now has the list gone empty?
     }
     else
     {
