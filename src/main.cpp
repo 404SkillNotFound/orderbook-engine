aa98b6b7f5680e428d110d08822f3a89eb5ffc9b
404SkillNotFound <shivang.upadhyay1@gmail.com>
2026-05-16 12:13:45 +0530

    add sequential trade test cases for matching engine

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,59 @@
 
 int main()
 {
-
     OrderBook o1;
+
+    // Buyer wants 50 shares at price 100
     Order buyOrder1;
-    buyOrder1.id = 001;
-    buyOrder1.price = 3000;
-    buyOrder1.quantity = 1200;
+    buyOrder1.id = 1;
+    buyOrder1.price = 100;
+    buyOrder1.quantity = 50;
     buyOrder1.side = 'B';
 
-    Order buyOrder2;
-    buyOrder2.id = 002;
-    buyOrder2.price = 40000;
-    buyOrder2.quantity = 100;
-    buyOrder2.side = 'S';
+    // Seller #1 sells 10 shares at 90
+    Order sellOrder1;
+    sellOrder1.id = 2;
+    sellOrder1.price = 90;
+    sellOrder1.quantity = 10;
+    sellOrder1.side = 'S';
 
-    Order buyOrder3;
-    buyOrder3.id = 003;
-    buyOrder3.price = 100;
-    buyOrder3.quantity = 10;
-    buyOrder3.side = 'S';
+    // Seller #2 sells 20 shares at 95
+    Order sellOrder2;
+    sellOrder2.id = 3;
+    sellOrder2.price = 95;
+    sellOrder2.quantity = 20;
+    sellOrder2.side = 'S';
 
-    Order buyOrder4;
-    buyOrder4.id = 004;
-    buyOrder4.price = 2000;
-    buyOrder4.quantity = 109;
-    buyOrder4.side = 'B';
+    // Seller #3 sells 15 shares at 100
+    Order sellOrder3;
+    sellOrder3.id = 4;
+    sellOrder3.price = 100;
+    sellOrder3.quantity = 15;
+    sellOrder3.side = 'S';
+
+    // This one should NOT trade
+    // because ask price > best bid
+    Order sellOrder4;
+    sellOrder4.id = 5;
+    sellOrder4.price = 120;
+    sellOrder4.quantity = 100;
+    sellOrder4.side = 'S';
 
     o1.addOrder(buyOrder1);
-    o1.addOrder(buyOrder2);
-    o1.addOrder(buyOrder3);
-    o1.addOrder(buyOrder4);
 
+    o1.addOrder(sellOrder1);
+    o1.addOrder(sellOrder2);
+    o1.addOrder(sellOrder3);
+    o1.addOrder(sellOrder4);
+
+    std::cout << "INITIAL ORDERBOOK\n";
     o1.printBook();
 
+    std::cout << "\nMATCHING ORDERS\n";
     o1.matchOrder();
+
+    std::cout << "\nFINAL ORDERBOOK\n";
+    o1.printBook();
 
     return 0;
 }
